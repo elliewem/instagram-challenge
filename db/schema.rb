d43b2e19ff68a37f74a8838d7b7a7836b2a46987
Ellie <elliewem@gmail.com>
2018-02-24 13:10:10 +0000

    Associate user and post

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224103836) do
+ActiveRecord::Schema.define(version: 20180224125709) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
