d769d52447ca1b4e342231c85e5dce62c961f58e
Ellie <elliewem@gmail.com>
2018-02-24 14:57:25 +0000

    Add comment model and associate comment with post

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,8 +25,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
+  # process :scale => [300, 300]
+  # #
   # def scale(width, height)
   #   # do something
   # end
